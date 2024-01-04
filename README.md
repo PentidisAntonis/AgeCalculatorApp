a0379fac7ff4020d5d50c72148329b8dabd74ee9
ahpen <a.h.pentidis@gmail.com>
2024-01-04 19:32:10 +0200

    Last Run and ReadMe file

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-pip3 install pyqt6
+Age Calculator
+
+**Description**
+
+This simple PyQt6 application allows users to calculate their age based on the provided name and date of birth (MM/DD/YYYY). The application uses a graphical user interface (GUI) to input the necessary information and display the calculated age.
+
+Dependencies
+
+Install Python
+
+If you don't have Python installed, you can download and install it from the official Python website: [Python Downloads](https://www.python.org/downloads/)
+
+Install PyQt6
+
+Make sure you have PyQt6 installed. You can install it using the following command:
+
+>pip install PyQt6
+
+**Usage**
+
+Run the script.
+Enter your name in the "Name" field.
+
+Enter your date of birth in the "Date of Birth MM/DD/YYYY" field.
+
+Click the "Calculate Age" button.
+
+The calculated age will be displayed below the button.
+
+**Code Overview**
+
+The code uses PyQt6 to create a simple GUI with the following components:
+
+**QLabel**: Display labels for "Name" and "Date of Birth MM/DD/YYYY".
+
+**QLineEdit**: Input fields for name and date of birth.
+
+**QPushButton**: Button to trigger the age calculation.
+
+**QGridLayout**: Organizes the layout of the widgets in a grid.
+
+**QWidget**: Base class for all UI objects.
+
+The calculate_age method uses the current year and the provided date of birth to calculate the age and updates the output label accordingly.
+
+**How to Run**
+
+Run the script using a Python interpreter. The application window will appear, and you can interact with it using the provided interface.
+
+>python your_script_name.py
+
+**Note**
+
+Ensure that PyQt6 is installed before running the script.
+
+Make sure to enter the date of birth in the specified format (MM/DD/YYYY) for accurate age calculation.
--- main.py
+++ main.py
@@ -1,29 +1,54 @@
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton
 
-##First commit
+# We used QGridLayout because it is more powerful than QVBoxLayout
+# (it only allows to position the widgets vertically stacked on top of one another)
+# QHBoxLayout (it only allows to position the widgets horizontally stacked on top of one another)
+
 
 import sys
 
+import sys
+from datetime import datetime
 
 class AgeCalculator(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Age Calculator")
         grid = QGridLayout()
 
         # create widgets
         name_label = QLabel("Name:")
-        name_line_edit = QLineEdit()
+        self.name_line_edit = QLineEdit()
 
         date_birth_label = QLabel("Date of Birth MM/DD/YYYY:")
-        date_birth_line_edit = QLineEdit()
+        self.date_birth_line_edit = QLineEdit()
+        #from local variable to intance variable so that I can accesses it below in the year_of_birth
+
+        # buttons
+        calculate_button = QPushButton("Calculate Age")
+        calculate_button.clicked.connect(self.calculate_age)
+        self.output_label = QLabel("")
 
         # Add widgets to grid
-        grid.addWidget(name_label, 0, 0)  # row, column 0.0
-        grid.addWidget(name_line_edit, 0, 1)  # row, column 0.1
-        grid.addWidget(date_birth_label, 1, 0)  # row, column 1.0
-        grid.addWidget(date_birth_line_edit, 1, 1)  # row, column 1.1
+        grid.addWidget(name_label, 0, 0)                    # row, column 0.0
+        grid.addWidget(self.name_line_edit, 0, 1)           # row, column 0.1
+
+        grid.addWidget(date_birth_label, 1, 0)              # row, column 1.0
+        grid.addWidget(self.date_birth_line_edit, 1, 1)     # row, column 1.1
+
+        grid.addWidget(calculate_button, 2, 0, 1, 2)  # row, column 2.0 span 1 row and 2 columns
+        grid.addWidget(self.output_label, 3, 0, 1, 2)  # row, column 3.0 span 1 row and 2 columns
+
 
         self.setLayout(grid)  ##this method is inherited by the QWidget
+
+    def calculate_age(self):
+        current_year = datetime.now().year
+        date_of_birth = self.date_birth_line_edit.text()  # to extract it
+        year_of_birth = datetime.strptime(date_of_birth,
+                                          "%m/%d/%Y").date().year  # we converted it in a datetime object and then we get the year
+        age = current_year - year_of_birth
+        self.output_label.setText(f"{self.name_line_edit.text()} is {age} years old.")  # setText is a method of a QLabel
 
 
 app = QApplication(sys.argv)
